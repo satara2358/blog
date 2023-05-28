f229e0a4e2b31dfaadf918e78471c3780d228d53
Satara <tatenokay@gmail.com>
2023-05-28 01:30:46 -0500

    Landing Test Astro COP

---
+++ src/Data/ItemsData.ts
@@ -0,0 +1,18 @@
+import type { MenuItem } from "../types/types";
+
+const menuItem:MenuItem [] = [
+  {
+    title: 'Home',
+    url : '/'
+  },
+  {
+    title: 'FAq',
+    url : '/faq'
+  },
+  {
+    title: 'Blog',
+    url : '/blog'
+  },
+];
+
+export default menuItem;
---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export interface MenuItem{
+
+  title: string;
+  url: string;
+}
+
+export interface CardItem {
+  title: string;
+  image: string;
+  description : string;
+}
